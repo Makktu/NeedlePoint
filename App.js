3980cc2ece5f7572acb771549dff20c0c549e6b7
Makktu <johnmcna@gmail.com>
2025-04-14 23:48:38 +0100

    setting up

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>NEEDLEPOINT</Text>
+      <Text>NEEDLEPOINT v0.1</Text>
       <StatusBar style='auto' />
     </View>
   );
